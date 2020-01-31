ee57dd56497104572fe8faac89031db6f56ac7f7
Sai Vinay <saivinayg.2011@gmail.com>
2020-01-30 23:16:21 -0700

    Roll a ball

---
+++ Roll a ball/Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+//@author : Sai Vinay Gangadharabhatla - sganga10
+//version : 1.0 
+public class Rotator : MonoBehaviour
+{
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+}
