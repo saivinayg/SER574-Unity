1606ca411091efd93b960fcbf53d7893ddcdb34f
Sai Vinay <saivinayg.2011@gmail.com>
2020-02-20 22:54:01 -0700

    Added Calculator

---
+++ Calculator/Assets/Cal.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.UI;
+public class Cal : MonoBehaviour
+{
+    public InputField n1,n2;
+    private int first,second;
+    public Text result;
+    public void onAdd()
+    {
+       first = int.Parse(n1.text);
+       second = int.Parse(n2.text);
+       result.text = "Result : " + (first+second).ToString();
+    }
+    public void onSub()
+    {
+        first = int.Parse(n1.text);
+        second = int.Parse(n2.text);
+        result.text = "Result : " + (first - second).ToString();
+    }
+    public void onMul()
+    {
+        first = int.Parse(n1.text);
+        second = int.Parse(n2.text);
+        result.text = "Result : " + (first * second).ToString();
+    }
+    public void onDiv()
+    {
+        first = int.Parse(n1.text);
+        second = int.Parse(n2.text);
+        result.text = "Result : " + (first / second).ToString();
+    }
+}
